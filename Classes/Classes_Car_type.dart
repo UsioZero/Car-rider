cdee8ea374306600add24b17c3b93e02bcbb331e
Usio <swortaartonlineforever@gmail.com>
2020-06-02 15:03:18 +0300

    v 2.4
    add encapsulation

--- Classes/Classes_Car_type.dart
+++ Classes/Classes_Car_type.dart
@@ -1,19 +1,19 @@
 import 'dart:math';
 
 class Car {
-  int maxFuel = 100;
+  int _maxFuel = 100;
   int fuel = 100;
-  int fuelPrice = 10;
+  int _fuelPrice = 10;
   int money = 10000;
   Car() {
     this.money = money;
     this.fuel = fuel;
-    this.fuelPrice = fuelPrice;
-    this.maxFuel = maxFuel;
+    this._fuelPrice = _fuelPrice;
+    this._maxFuel = _maxFuel;
   }
   void refilling() {
-    this.money -= this.fuelPrice * (this.maxFuel - this.fuel);
-    this.fuel = this.maxFuel;
+    this.money -= this._fuelPrice * (this._maxFuel - this.fuel);
+    this.fuel = this._maxFuel;
   }
 
   void status() {
@@ -22,16 +22,16 @@ class Car {
 }
 
 class Truck extends Car {
-  int fuelkm = 2;
+  int _fuelkm = 2;
   int moneykm = 30;
   Truck() {
     super.money -= 4000;
-    this.fuelkm = fuelkm;
+    this._fuelkm = _fuelkm;
     this.moneykm = moneykm;
   }
   void ride() {
-    if (super.fuel >= this.fuelkm * 10) {
-      super.fuel -= this.fuelkm * 10;
+    if (super.fuel >= this._fuelkm * 10) {
+      super.fuel -= this._fuelkm * 10;
       int min = 10;
       int max = 40;
       Random r = new Random();
@@ -44,16 +44,16 @@ class Truck extends Car {
 }
 
 class Racing extends Car {
-  int fuelkm = 4;
+  int _fuelkm = 4;
   int moneykm = 70;
   Racing() {
     super.money -= 6000;
-    this.fuelkm = fuelkm;
+    this._fuelkm = _fuelkm;
     this.moneykm = moneykm;
   }
   void ride() {
-    if (super.fuel >= this.fuelkm * 10) {
-      super.fuel -= this.fuelkm * 10;
+    if (super.fuel >= this._fuelkm * 10) {
+      super.fuel -= this._fuelkm * 10;
       int min = 10;
       int max = 90;
       Random r = new Random();
